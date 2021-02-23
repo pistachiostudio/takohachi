27614f08f85daa54bb846fb3ed83cf6ba9ae7144
guojama <guojama@gmail.com>
2021-02-24 00:50:32 +0900

    yo bro

--- launcher.py
+++ launcher.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import config
+import asyncio
 
 bot = commands.Bot(command_prefix="!")
 
@@ -19,7 +20,20 @@ async def on_message(message):
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send(f"こんにちわ、{ctx.author.name}さん。")
+    
+    def check_message_author(msg):
+        return msg.author is ctx.author
+    
+    await ctx.send(f" こんにちは、{ctx.author.name} さん。")
+    await ctx.send(f"気分はどうですか？")
+
+    try:
+        msg = await bot.wait_for('message', check=check_message_author, timeout=10)
+    except asyncio.TimeoutError:
+        await ctx.send("タイムアウトしました。")
+        return
+
+    await ctx.send(f"「{msg.content}」という気分なんだ。")
 
 
 bot.run(config.TOKEN)
