d8969561f3aa0de2adba7797bdd58fc40c328769
guojama <guojama@gmail.com>
2021-02-26 00:47:29 +0900

    yo command add

--- launcher.py
+++ launcher.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import discord
 import config
 
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="!!")
 
 @bot.event
 async def on_ready():
@@ -14,14 +14,21 @@ async def on_message(message):
         # Botからのメッセージには反応しない
         # この判定をしないと無限ループが起きる
         return
-
     DIS_WORDS = ['バカ', 'ばか', 'あほ', '死ね', 'だめ', 'ゴミ']
-
+    
     # 全NGワードについて存在確認
     for dis_word in DIS_WORDS:
         if dis_word in message.content:
             # NGワードを発見したらテキストチャンネルに通知
             await message.channel.send(f"{dis_word}野郎はテメェだ！")
-            break
+
+    if "bot" in message.content:
+        await message.channel.send("はーい助六ボットです。")
+
+    await bot.process_commands(message)
+
+@bot.command()
+async def yo(ctx):
+    await ctx.send(f"Yo bro, {ctx.author.name} dude.")
             
 bot.run(config.TOKEN)
