fb225410e103f9cee275c73d028c776238bf4cd2
guojama <guojama@gmail.com>
2021-02-26 00:10:27 +0900

    0225comitter

--- launcher.py
+++ launcher.py
@@ -1,15 +1,12 @@
 from discord.ext import commands
+import discord
 import config
-import asyncio
-import discord  # 追加
 
 bot = commands.Bot(command_prefix="!")
-
 
 @bot.event
 async def on_ready():
     print("on_ready")
-
 
 @bot.event
 async def on_message(message):
@@ -18,38 +15,13 @@ async def on_message(message):
         # この判定をしないと無限ループが起きる
         return
 
-    if "Bot" in message.content:
-        await message.channel.send("は～い、Botで～す")
+    DIS_WORDS = ['バカ', 'ばか', 'あほ', '死ね', 'だめ', 'ゴミ']
 
-    # 下記の処理を追加
-    await bot.process_commands(message)
-
-
-@bot.command()
-async def hello(ctx):
-    # 待機するメッセージのチェック関数
-    def check_message_author(msg):
-        return msg.author is ctx.author
-    # あいさつする既存の処理
-    await ctx.send(f"こんにちは、{ctx.author.name}さん。")
-    await ctx.send("ご気分はいかがでしょうか？")
-    # チェック関数に合格するようなメッセージを待つ
-    try:
-        # チェック関数に合格するようなメッセージを待つ
-        msg = await bot.wait_for('message', check=check_message_author, timeout=10)
-    except asyncio.TimeoutError:
-        await ctx.send("タイムアウトしました。")
-        return
-    # 受け取ったメッセージの内容を使って返信
-    # Embedインスタンスを作成
-    embed = discord.Embed()
-    # Embedの表示色を青色に設定
-    embed.color = discord.Color.blue()
-    # Embedの説明文を設定
-    embed.description = "あなたの気分を把握しました。"
-    # 気分をFieldとして表示
-    embed.add_field(name="あなたの気分", value=msg.content)
-    await ctx.send(embed=embed)
-
-
+    # 全NGワードについて存在確認
+    for dis_word in DIS_WORDS:
+        if dis_word in message.content:
+            # NGワードを発見したらテキストチャンネルに通知
+            await message.channel.send(f"{dis_word}野郎はテメェだ！")
+            break
+            
 bot.run(config.TOKEN)
