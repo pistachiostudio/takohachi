41bb3fc5ae2550359a8cd026fd1c778219e473fd
guojama <guojama@gmail.com>
2021-03-18 02:59:32 +0900

    aaa

--- cogs/vcalert.py
+++ cogs/vcalert.py
@@ -9,36 +9,24 @@ class VC_alert(commands.Cog):
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        VC_ID_01 = self.bot.get_channel(762575797327757322)
-        VC_ID_02 = self.bot.get_channel(762576631810228256)
-        VC_ID_03 = self.bot.get_channel(780611246155497482)
-        VC_ID_04 = self.bot.get_channel(812312211112198144)
-        VC_ID_05 = self.bot.get_channel(819874983290339338)
+        
+        #省きたいチャンネルidを入力
+        not_01 = self.bot.get_channel(762575797327757322)
+        not_02 = self.bot.get_channel(762576631810228256)
+        not_03 = self.bot.get_channel(780611246155497482)
+        not_04 = self.bot.get_channel(812312211112198144)
+        not_05 = self.bot.get_channel(819874983290339338)
 
-        if after.channel is VC_ID_01 and not None:
-            channel_id = 821790152085995561
-            text_channel = self.bot.get_channel(channel_id)
-            await text_channel.send(f"**{member.name}** が **{after.channel.name}** に入りました。")
+        #除外チャンネルの場合はreturn
+        if after.channel is not_01 or after.channel is not_02 or after.channel is not_03 or after.channel is not_04 or after.channel is not_05:
+            return
 
-        elif after.channel is VC_ID_02 and not None:
-            channel_id = 821790152085995561
-            text_channel = self.bot.get_channel(channel_id)
-            await text_channel.send(f"**{member.name}** が **{after.channel.name}** に入りました。")
-
-        elif after.channel is VC_ID_03 and not None:
-            channel_id = 821790152085995561
-            text_channel = self.bot.get_channel(channel_id)
-            await text_channel.send(f"**{member.name}** が **{after.channel.name}** に入りました。")
-
-        elif after.channel is VC_ID_04 and not None:
-            channel_id = 821790152085995561
-            text_channel = self.bot.get_channel(channel_id)
-            await text_channel.send(f"**{member.name}** が **{after.channel.name}** に入りました。")
-
-        elif after.channel is VC_ID_05 and not None:
-            channel_id = 821790152085995561
-            text_channel = self.bot.get_channel(channel_id)
-            await text_channel.send(f"**{member.name}** が **{after.channel.name}** に入りました。")            
+        else:
+            if before.channel is None and after.channel is not None:
+                #メッセージを送るテキストチャンネルID
+                channel_id = 821804359700185088
+                text_channel = self.bot.get_channel(channel_id)
+                await text_channel.send(f"**{member.name}** が **{after.channel.name}** に入りました。")
 
 def setup(bot):
     bot.add_cog(VC_alert(bot))
