b293afdbd5095feccc9eea983544adb5536642bf
guojama <guojama@gmail.com>
2021-03-19 22:54:54 +0900

    ggg

--- cogs/vctest.py
+++ cogs/vctest.py
@@ -9,16 +9,16 @@ class VC_test(commands.Cog):
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        
+
         #省きたいチャンネルidを入力
         not_01 = self.bot.get_channel(803245850616791040) #music club vc
         not_02 = self.bot.get_channel(801064070399787028) #system vc
-        #not_03 = self.bot.get_channel()
+        not_03 = self.bot.get_channel(815042744534958130) #drakee
         #not_04 = self.bot.get_channel()
         #not_05 = self.bot.get_channel()
         
         #除外チャンネルの場合はreturn
-        if after.channel in [not_01, not_02]:
+        if after.channel in [not_01, not_02, not_03]:
             return
 
         else:
