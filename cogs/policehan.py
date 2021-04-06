e5ba6621ea2fead30d22154ab62b27f5ab3b632c
guojama <guojama@gmail.com>
2021-04-06 22:13:47 +0900

    add han

---
+++ cogs/policehan.py
@@ -0,0 +1,30 @@
+from discord.ext import commands
+import config
+import asyncio
+import discord
+
+class Hankaku(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+        # Botからのメッセージには反応しない
+        # この判定をしないと無限ループが起きる
+            return
+        
+        HANWORDS = ['ｦ', 'ｧ', 'ｨ', 'ｩ', 'ｪ', 'ｫ', 'ｬ', 'ｭ', 'ｮ', 'ｯ', 'ｰ', 'ｱ', 'ｲ', 'ｳ', 'ｴ', 'ｵ', 'ｶ', 'ｷ', 'ｸ', 'ｹ', 'ｺ', 'ｻ', 'ｼ', 'ｽ', 'ｾ', 'ｿ', 'ﾀ', 'ﾁ', 'ﾂ', 'ﾃ', 'ﾄ', 'ﾅ', 'ﾆ', 'ﾇ', 'ﾈ', 'ﾉ', 'ﾊ', 'ﾋ', 'ﾌ', 'ﾍ', 'ﾎ', 'ﾏ', 'ﾐ', 'ﾑ', 'ﾒ', 'ﾓ', 'ﾔ', 'ﾕ', 'ﾖ', 'ﾗ', 'ﾘ', 'ﾙ', 'ﾚ', 'ﾛ', 'ﾜ', 'ﾝ', 'ﾞ', 'ﾟ']
+    
+        # 全NGワードについて存在確認
+        for hankaku in HANWORDS:
+            if hankaku in message.content:
+            # NGワードを発見したらテキストチャンネルに通知
+                for reaction in [":p01_text_han:828978524596731937", ":p01_text_kaku:828979073694040084", ":p01_text_ch4:808200593813274644", ":p01_text_ch13:808515343957098496", "👀"]:
+                    await message.add_reaction(reaction)
+                
+    #await self.bot.process_commands(message) これがcogでは不要になる！！
+
+
+def setup(bot):
+    bot.add_cog(Hankaku(bot))
