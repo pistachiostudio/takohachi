4921aa15c6772dd0ca806401c622c18792f9f9af
guojama <giantkoyama@gmail.com>
2021-04-07 17:43:51 +0900

    Update policehan.py

--- cogs/policehan.py
+++ cogs/policehan.py
@@ -24,7 +24,7 @@ class Hankaku(commands.Cog):
                     await message.add_reaction(reaction)
                    
                 
-        GOMIWORDS = ['ごみ', 'ゴミ', '53', '５３']
+        GOMIWORDS = ['gomi', 'GOMI', 'Gomi', 'ごみ', 'ゴミ', '53', '５３']
     
         # 全NGワードについて存在確認
         for gomi in GOMIWORDS:
