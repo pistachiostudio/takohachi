f1182f6df76dea30bdb6bc376a8444e7140c5b48
guojama <guojama@gmail.com>
2021-09-28 22:53:42 +0900

    marimo timeにpaul timeを追加

--- cogs/marimo.py
+++ cogs/marimo.py
@@ -10,10 +10,12 @@ class Marimo(commands.Cog):
     @commands.command()
     async def mt(self, ctx):
         now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=-4)))
+        pnow = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+1)))
         JST = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+9)))
         marimo_time = f"{now.month}/{now.day} {now.hour}:{now.minute:02}"
+        paul_time = f"{pnow.month}/{pnow.day} {pnow.hour}:{pnow.minute:02}"
         japan_time = f"{JST.month}/{JST.day} {JST.hour}:{JST.minute:02}"
-        await ctx.send(f"marimo time = **{marimo_time}**\n(In Japan = {japan_time})")
+        await ctx.send(f"marimo time = **{marimo_time}**\npaul time = **{paul_time}**\n(In Japan = {japan_time})")
 
 
 def setup(bot):
