eaed89ca9ad3ff800be59c3ff88ab3330db5ad28
quojama <giantkoyama@gmail.com>
2021-10-02 00:49:59 +0900

    help_command=None >(+_+;)

--- launcher.py
+++ launcher.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!!")
+bot = commands.Bot(command_prefix="!!", help_command=None)
 
 # 環境変数からトークンを読み込む
 TOKEN = os.environ["TOKEN"]
