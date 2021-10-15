71c2b2e2b1e098e1b820ce22808be26bbff9024d
quojama <giantkoyama@gmail.com>
2021-10-16 03:46:07 +0900

    fix enter method

--- cogs/commandslist.py
+++ cogs/commandslist.py
@@ -16,9 +16,9 @@ class Help(commands.Cog):
         embed.color = discord.Color.blue()
         embed.description = "Prefix は `!!` です。\n\
                             [View more info on GitHub](https://github.com/pistachiostudio/takohachi/blob/master/mannual.md)\n\n\
-                            **mt**\n```まりもたいむ！```\n\n**whatToday**\n```今日はなんの日？```\n\n**apexrank <A> <B>**\n\
-                            ```APEXのランクポイントを表示します。\nA = origin or psn or xbl\nB = YourID```\n\n\
-                            **sp <SEARCH>**\n```Spotifyの曲情報をゲットします。```\n\n**spartist <ARTIST>**\n```Spotifyのアーティスト情報をゲットします。```\n\n\
+                            **mt**\n```まりもたいむ！```\n**whatToday**\n```今日はなんの日？```\n**apexrank <A> <B>**\n\
+                            ```APEXのランクポイントを表示します。\nA = origin or psn or xbl\nB = YourID```\n\
+                            **sp <SEARCH>**\n```Spotifyの曲情報をゲットします。```\n**spartist <ARTIST>**\n```Spotifyのアーティスト情報をゲットします。```\n\
                             **addssl <URL>**\n```SSL Checkerのデータベースに新しい監視URLを登録します。```\n**count**\n```コマンドを書き込んだチャンネルの現在のメッセージの総件数を返します。```\n\
                             [SSL Checker](https://ssl-checker.vercel.app) ｜ [SSLC database](https://docs.google.com/spreadsheets/d/1c25pvMyjQ89OBCvB9whCQQLM_BPXKyY7umsj5wmpP2k/edit?usp=sharing)"
 
