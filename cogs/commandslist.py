379deba944751463f77fe19f020fadac7575e42d
quojama <giantkoyama@gmail.com>
2022-01-21 02:06:41 +0900

    fix order

--- cogs/commandslist.py
+++ cogs/commandslist.py
@@ -34,11 +34,11 @@ B = YourID```
         ```コマンドを書き込んだチャンネルの現在のメッセージの総件数を返します。```
         **countall**
         ```犬～恐竜の4チャンネルの現在のメッセージの合計を返します。```
-        **addssl <URL>**
         **card**
         ```イエローカードやレッドカードの集計結果のベスト5を返します。```
         **cardall**
         ```イエローカードやレッドカードのすべての集計結果を返します。```
+        **addssl <URL>**
         ```SSL Checkerのデータベースに新しい監視URLを登録します。```
         [SSL Checker](https://ssl-checker.vercel.app) ｜ [SSLC database](https://docs.google.com/spreadsheets/d/1c25pvMyjQ89OBCvB9whCQQLM_BPXKyY7umsj5wmpP2k/edit?usp=sharing)"
         """
