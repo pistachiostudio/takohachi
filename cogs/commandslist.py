52bacdd4ddbe801565bba931fb9c1f6714cf0717
quojama <giantkoyama@gmail.com>
2022-01-21 02:03:32 +0900

    fix order

--- cogs/commandslist.py
+++ cogs/commandslist.py
@@ -35,12 +35,12 @@ B = YourID```
         **countall**
         ```犬～恐竜の4チャンネルの現在のメッセージの合計を返します。```
         **addssl <URL>**
-        ```SSL Checkerのデータベースに新しい監視URLを登録します。```
-        [SSL Checker](https://ssl-checker.vercel.app) ｜ [SSLC database](https://docs.google.com/spreadsheets/d/1c25pvMyjQ89OBCvB9whCQQLM_BPXKyY7umsj5wmpP2k/edit?usp=sharing)"
         **card**
         ```イエローカードやレッドカードの集計結果のベスト5を返します。```
         **cardall**
         ```イエローカードやレッドカードのすべての集計結果を返します。```
+        ```SSL Checkerのデータベースに新しい監視URLを登録します。```
+        [SSL Checker](https://ssl-checker.vercel.app) ｜ [SSLC database](https://docs.google.com/spreadsheets/d/1c25pvMyjQ89OBCvB9whCQQLM_BPXKyY7umsj5wmpP2k/edit?usp=sharing)"
         """
 
         await ctx.send(embed=embed)
