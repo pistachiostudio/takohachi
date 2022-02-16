62351d98da851570e4005ac8a155fb840874c8c7
guojama <giantkoyama@gmail.com>
2022-02-16 13:36:50 +0900

    add dic

--- cogs/commandslist.py
+++ cogs/commandslist.py
@@ -38,6 +38,8 @@ B = YourID```
         ```イエローカードやレッドカードの集計結果のベスト5を返します。```
         **cardall**
         ```イエローカードやレッドカードのすべての集計結果を返します。```
+        **dic <WORDS>**
+        ```<WORDS>がデータベースに登録されていればその意味を返します。```
         **addssl <URL>**
         ```SSL Checkerのデータベースに新しい監視URLを登録します。```
         [SSL Checker](https://ssl-checker.vercel.app) ｜ [SSLC database](https://docs.google.com/spreadsheets/d/1c25pvMyjQ89OBCvB9whCQQLM_BPXKyY7umsj5wmpP2k/edit?usp=sharing)"
