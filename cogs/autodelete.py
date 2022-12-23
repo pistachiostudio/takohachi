e3adad703797a9659771f4707aa0a3d030b5a3fb
quojama <giantkoyama@gmail.com>
2022-12-24 00:37:21 +0900

    これでいけるのか

--- cogs/autodelete.py
+++ cogs/autodelete.py
@@ -14,18 +14,18 @@ class AutoDelete(commands.Cog):
     def cog_unload(self):
         self.printer.cancel()
 
-    @tasks.loop(seconds=600.0)
+    @tasks.loop(seconds=10.0)
     async def printer(self):
-        channel = self.bot.get_channel(int('780611197350576200'))
-
-        now = int(time.time())
-
-        counter = 0
-        async for message in channel.history():
-            message_time = int(message.created_at.timestamp())
-            if now-message_time > 43200:
-                await message.delete()
-            counter += 1
+        channel_list = ["xxxxxxxxxxxxx", "cccccccccccccccc"]
+        for channel_id in channel_list:
+            channel = self.bot.get_channel(int(channel_id))
+            now = int(time.time())
+            counter = 0
+            async for message in channel.history(oldest_first=True):
+                message_time = int(message.created_at.timestamp())
+                if now-message_time > 60:
+                    await message.delete()
+                counter += 1
 
     #デプロイ後Botが完全に起動してからタスクを回す
     @printer.before_loop
