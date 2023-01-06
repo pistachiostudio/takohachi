bfad046dd7728faf32dccd6c1971a132561d5c51
zztkm <takumi.basket1682@gmail.com>
2023-01-06 22:50:30 +0900

    update

---
+++ tests/test_libs/test_channel.py
@@ -0,0 +1,14 @@
+from discord.ext.commands import Context
+
+def test_get_channels():
+    # Given
+    from libs.channel import get_channels
+
+    get_channels
+    ctx
+
+    # When
+    res = get_channels
+
+    # Then
+
