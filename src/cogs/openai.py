d43f8ffde5d0c10f6a6e63c8a29736e4f411f53b
quojama <giantkoyama@gmail.com>
2023-03-03 00:17:32 +0900

    introducing gpt-3.5-turbo with async

--- src/cogs/openai.py
+++ src/cogs/openai.py
@@ -1,8 +1,7 @@
 import os
 
-import aiohttp
 import discord
-import openai_async as openai
+import httpx
 from discord import app_commands
 from discord.ext import commands
 
@@ -16,45 +15,56 @@ class Openai(commands.Cog):
     @app_commands.command(
         name="gpt",
         description="ChatGPTに質問をしましょう！"
-    )
+        )
+
     @app_commands.describe(
-        key="ChatGPTに質問をどうぞ！"
-    )
+        key="質問内容",
+        character="ChatGPTに性格やキャラを与えることができます。必ず「あなたは～です。」と書いてください。"
+        )
+
     async def openai(
         self,
         interaction: discord.Interaction,
-        key: str
-    ):
+        key: str,
+        character:str = "あなたは、OpenAI によってトレーニングされた大規模な言語モデルである ChatGPT です。できるだけ簡潔に答えてください。知識のカットオフ: {knowledge_cutoff} 現在の日付: {current_date}"
+        ):
 
         await interaction.response.defer()
 
-        openai.api_key = os.getenv("OPENAI_API_KEY")
+        endpoint = "https://api.openai.com/v1/chat/completions"
 
-        response = await openai.complete(
-            openai.api_key,
-            timeout=60,
-            payload={
-                "model": "text-davinci-003",
-                "prompt": key,
-                "max_tokens": 1000,
-                "temperature": 0.7,
-            },
-        )
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {os.getenv("OPENAI_API_KEY")}'
+        }
 
-        answer = response.json()["choices"][0]["text"].strip()
-        tokens = response.json()['usage']['total_tokens']
-        cost = round(tokens*0.00002*get_exchange_rate(), 3)
+        payload = {
+            "model": "gpt-3.5-turbo",
+            "messages" : [
+                {"role": "system", "content": character},
+                {"role": "user", "content": key}
+            ],
+            "max_tokens": 1000
+        }
+
+        async with httpx.AsyncClient() as client:
+            res = await client.post(endpoint, headers=headers, json=payload)
+        json = res.json()
+
+        answer = json["choices"][0]["message"]["content"]
+        tokens = json["usage"]["total_tokens"]
+        cost = round(tokens * 0.000002 * get_exchange_rate(), 3)
+
+        if character == "あなたは、OpenAI によってトレーニングされた大規模な言語モデルである ChatGPT です。できるだけ簡潔に答えてください。知識のカットオフ: {knowledge_cutoff} 現在の日付: {current_date}":
+            character = "Default"
 
         embed = discord.Embed()
         embed.title = f"Q. {key}"
         embed.description = answer
         embed.color = discord.Color.dark_green()
-        embed.set_footer(text=f"💸この質問の料金は {cost}円 でした。")
+        embed.set_footer(text=f"🤖 キャラ設定: {character}\n💸 この質問の料金は {cost}円 でした。")
 
         await interaction.followup.send(embed=embed)
 
 async def setup(bot: commands.Bot):
-    await bot.add_cog(
-        Openai(bot),
-        guilds = [discord.Object(id=731366036649279518)]
-    )
+    await bot.add_cog(Openai(bot), guilds=[discord.Object(id=731366036649279518)])
