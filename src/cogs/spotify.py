c05d0297a6d28b6332e33f581e196bf4e98e4571
quojama <giantkoyama@gmail.com>
2023-03-03 22:09:58 +0900

    add album serch on spotify.py

--- src/cogs/spotify.py
+++ src/cogs/spotify.py
@@ -12,21 +12,31 @@ class Spotify(commands.Cog):
         self.bot = bot
 
     @app_commands.command(
-        name="sp",
+        name="spotify",
         description="Spotifyの曲情報を検索します。",
     )
+
     @app_commands.describe(
-        search="曲情報を表示します。アーティスト名や曲名で絞り込んでください。スペース区切りOK。"
+        select="選択してください。",
+        search="検索ワードを入力してください。"
     )
+
+    @app_commands.choices(
+        select=[
+            discord.app_commands.Choice(name="Song",value="song"),
+            discord.app_commands.Choice(name="Artist",value="artist"),
+            discord.app_commands.Choice(name="Album",value="album")
+        ]
+    )
+
     async def sp(
         self,
         interaction: discord.Interaction,
+        select: str,
         search: str
     ):
-        # interactionは3秒以内にレスポンスしないといけないとエラーになるのでこの処理を入れる。
-        await interaction.response.defer()
 
-        #arguments = ' '.join(search)
+        # arguments = ' '.join(args)
         arguments = search
 
         # 起動
@@ -35,140 +45,151 @@ class Spotify(commands.Cog):
         client_credentials_manager = spotipy.oauth2.SpotifyClientCredentials(client_id, client_secret)
         spo = spotipy.Spotify(client_credentials_manager=client_credentials_manager, language='ja')
 
-        # searchから曲名を取る
-        searchtrack = spo.search(q=f"{arguments}", type="track", market="JP", limit=1)
-        for idx, track in enumerate(searchtrack['tracks']['items']):
-            songname = track['name']
+        # Songの検索の場合
+        if select == "song":
 
-        # searchから曲のidを取る
-        for track in searchtrack['tracks']['items']:
-            song_id = track['id']
+            # interactionは3秒以内にレスポンスしないといけないとエラーになるのでこの処理を入れる。
+            await interaction.response.defer()
 
-        # アーティスト情報
-        trackinfo = spo.track(song_id)
-        arname = [d.get('name') for d in trackinfo['artists']]
-        arname = ', '.join(arname)
+            # searchから曲名を取る
+            searchtrack = spo.search(q=f"{arguments}", type="track", market="JP", limit=1)
+            for idx, track in enumerate(searchtrack['tracks']['items']):
+                songname = track['name']
 
-        # ジャケ
-        track = trackinfo['album']['images'][0]
-        imageurl = track['url']
+            # searchから曲のidを取る
+            for track in searchtrack['tracks']['items']:
+                song_id = track['id']
 
-        # Popularity
-        for track in trackinfo['artists']:
-            popularities = trackinfo['popularity']
+            # アーティスト情報
+            trackinfo = spo.track(song_id)
+            arname = [d.get('name') for d in trackinfo['artists']]
+            arname = ', '.join(arname)
 
-        # track url
-        trackurl = trackinfo['external_urls']['spotify']
+            # ジャケ
+            track = trackinfo['album']['images'][0]
+            imageurl = track['url']
 
-        # 曲のidをfeatureに渡した
-        feature = spo.audio_features(song_id)
-        for item in feature:
-            loudness = round(item['loudness'], 1)
-            key = item['key']
-            bpm = round(item['tempo'], 1)
-            time_signature = item['time_signature']
-            danceability = round(item['danceability'] * 100, 1)
-            energy = round(item['energy'] * 100, 1)
-            acousticness = round(item['acousticness'] * 100, 1)
-            liveness = round(item['liveness'] * 100, 1)
-            inst = round(item['instrumentalness'] * 100, 1)
-            duration_ms = item['duration_ms']
-            duration = (duration_ms / 60000)
-            mode = item['mode']
+            # Popularity
+            for track in trackinfo['artists']:
+                popularities = trackinfo['popularity']
 
-        # key変換
-        keydic = {0:"C", 1:"C#", 2:"D", 3:"D#", 4:"E", 5:"F", 6:"F#", 7:"G", 8:"G#", 9:"A", 10:"A#", 11:"B"}
-        keys = keydic[key]
+            # track url
+            trackurl = trackinfo['external_urls']['spotify']
 
-        # mojor, minor変換
-        modedic = {0:"Minor", 1:"Major"}
-        majmin = modedic[mode]
+            # 曲のidをfeatureに渡した
+            feature = spo.audio_features(song_id)
+            for item in feature:
+                loudness = round(item['loudness'], 1)
+                key = item['key']
+                bpm = round(item['tempo'], 1)
+                time_signature = item['time_signature']
+                danceability = round(item['danceability'] * 100, 1)
+                energy = round(item['energy'] * 100, 1)
+                acousticness = round(item['acousticness'] * 100, 1)
+                liveness = round(item['liveness'] * 100, 1)
+                inst = round(item['instrumentalness'] * 100, 1)
+                duration_ms = item['duration_ms']
+                duration = (duration_ms / 60000)
+                mode = item['mode']
 
-        # embed
-        embed = discord.Embed()
-        JST = timezone(timedelta(hours=+9), "JST")
-        embed.timestamp = datetime.now(JST)
-        embed.title = "Spotify song analizer..."
-        embed.color = discord.Color.red()
-        embed.description = f"**Track:** {songname}\n**Artist:** {arname}\n[Listen this track!]({trackurl})"
-        embed.set_thumbnail(url=imageurl)
-        embed.add_field(name="Popularity", value=f"```{popularities}```")
-        embed.add_field(name="Key", value=f"```{keys}```")
-        embed.add_field(name="Mode", value=f"```{majmin}```")
-        embed.add_field(name="BPM", value=f"```{bpm}```")
-        embed.add_field(name="TS", value=f"```{time_signature}/4```")
-        embed.add_field(name="Loudness", value=f"```{loudness}db```")
-        embed.add_field(name="Danceability", value=f"```{danceability}%```")
-        embed.add_field(name="Energy", value=f"```{energy}%```")
-        embed.add_field(name="Acousticness", value=f"```{acousticness}%```")
-        embed.add_field(name="Liveness", value=f"```{liveness}%```")
-        embed.add_field(name="Instrumentalness", value=f"```{inst}%```")
+            # key変換
+            keydic = {0:"C", 1:"C#", 2:"D", 3:"D#", 4:"E", 5:"F", 6:"F#", 7:"G", 8:"G#", 9:"A", 10:"A#", 11:"B"}
+            keys = keydic[key]
 
-        await interaction.followup.send(embed=embed)
+            # mojor, minor変換
+            modedic = {0:"Minor", 1:"Major"}
+            majmin = modedic[mode]
 
-    @app_commands.command(
-        name="spartist",
-        description="Spotifyのアーティスト情報を検索します。",
-    )
+            # embed
+            embed = discord.Embed()
+            JST = timezone(timedelta(hours=+9), "JST")
+            embed.timestamp = datetime.now(JST)
+            embed.title = "Spotify song analyser..."
+            embed.color = discord.Color.red()
+            embed.description = f"**Track:** {songname}\n**Artist:** {arname}\n[Listen this track!]({trackurl})"
+            embed.set_thumbnail(url=imageurl)
+            embed.add_field(name="Popularity", value=f"```{popularities}```")
+            embed.add_field(name="Key", value=f"```{keys}```")
+            embed.add_field(name="Mode", value=f"```{majmin}```")
+            embed.add_field(name="BPM", value=f"```{bpm}```")
+            embed.add_field(name="TS", value=f"```{time_signature}/4```")
+            embed.add_field(name="Loudness", value=f"```{loudness}db```")
+            embed.add_field(name="Danceability", value=f"```{danceability}%```")
+            embed.add_field(name="Energy", value=f"```{energy}%```")
+            embed.add_field(name="Acousticness", value=f"```{acousticness}%```")
+            embed.add_field(name="Liveness", value=f"```{liveness}%```")
+            embed.add_field(name="Instrumentalness", value=f"```{inst}%```")
+            await interaction.followup.send(embed=embed)
 
-    @app_commands.describe(artist="アーティスト情報を返します。アーティスト名を入力してください。")
+        # Artistの検索の場合
+        elif select == "artist":
+            # interactionは3秒以内にレスポンスしないといけないとエラーになるのでこの処理を入れる。
+            await interaction.response.defer()
 
-    async def spartist(
-        self,
-        interaction: discord.Interaction,
-        artist: str
-    ):
-        # interactionは3秒以内にレスポンスしないといけないとエラーになるのでこの処理を入れる。
-        await interaction.response.defer()
+            # 検索ワード
+            searchartist = spo.search(q=f"{arguments}", type="artist", market="JP", limit=1)
 
-        # arguments = ' '.join(args)
-        arguments = artist
+            # searchからアーティスト名を取る
+            for idx, track in enumerate(searchartist['artists']['items']):
+                artistname = track['name']
 
-        # 起動
-        client_id = os.environ["SPOTIFY_CLIENT_ID"]
-        client_secret = os.environ["SPOTIFY_CLIENT_SECRET"]
-        client_credentials_manager = spotipy.oauth2.SpotifyClientCredentials(client_id, client_secret)
-        spo = spotipy.Spotify(client_credentials_manager=client_credentials_manager, language='ja')
+            # searchからアーティストidを取る
+            for track in searchartist['artists']['items']:
+                artist_id = track['id']
 
-        # 検索ワード
-        searchartist = spo.search(q=f"{arguments}", type="artist", market="JP", limit=1)
+            # ジャンルをすべて取る
+            artistinfo = spo.artist(artist_id)
+            artistgenre = artistinfo['genres']
+            argenre = ', '.join(artistgenre)
 
-        # searchからアーティスト名を取る
-        for idx, track in enumerate(searchartist['artists']['items']):
-            artistname = track['name']
+            #アー写の一番でかいやつ
+            track = artistinfo['images'][0]
+            imgurl = track['url']
 
-        # searchからアーティストidを取る
-        for track in searchartist['artists']['items']:
-            artist_id = track['id']
+            #フォロワー
+            follower = '{:,}'.format(artistinfo['followers']['total'])
 
-        # ジャンルをすべて取る
-        artistinfo = spo.artist(artist_id)
-        artistgenre = artistinfo['genres']
-        argenre = ', '.join(artistgenre)
+            # Popularity
+            artistpopularity = artistinfo['popularity']
 
-        #アー写の一番でかいやつ
-        track = artistinfo['images'][0]
-        imgurl = track['url']
+            # artist url
+            artisturl = artistinfo['external_urls']['spotify']
 
-        #フォロワー
-        follower = '{:,}'.format(artistinfo['followers']['total'])
+            # artist embed
+            embed = discord.Embed()
+            JST = timezone(timedelta(hours=+9), "JST")
+            embed.timestamp = datetime.now(JST)
+            embed.title = f"{artistname}'s Profile"
+            embed.color = discord.Color.green()
+            embed.description = f"**Popularity:** {artistpopularity}\n**Followers:** {follower}\n**Genre:** {argenre}\n[Listen this artist!]({artisturl})"
+            embed.set_image(url=imgurl)
+            await interaction.followup.send(embed=embed)
 
-        # Popularity
-        artistpopularity = artistinfo['popularity']
+        # Albumの検索の場合
+        elif select == "album":
+            # interactionは3秒以内にレスポンスしないといけないとエラーになるのでこの処理を入れる。
+            await interaction.response.defer()
 
-        # artist url
-        artisturl = artistinfo['external_urls']['spotify']
+            # 検索ワード
+            searchalbum = spo.search(q=f"{arguments}", type="album", market="JP", limit=1)
 
-        # artist embed
-        embed = discord.Embed()
-        JST = timezone(timedelta(hours=+9), "JST")
-        embed.timestamp = datetime.now(JST)
-        embed.title = f"{artistname}'s Profile"
-        embed.color = discord.Color.green()
-        embed.description = f"**Popularity:** {artistpopularity}\n**Followers:** {follower}\n**Genre:** {argenre}\n[Listen this artist!]({artisturl})"
-        embed.set_image(url=imgurl)
+            # 諸々情報取得
+            album_title = searchalbum['albums']['items'][0]['name']
+            album_release_date = searchalbum['albums']['items'][0]['release_date']
+            album_artist_name = searchalbum['albums']['items'][0]['artists'][0]['name']
+            album_artist_url = searchalbum['albums']['items'][0]['artists'][0]['external_urls']['spotify']
+            album_url = searchalbum['albums']['items'][0]['external_urls']['spotify']
+            album_image = searchalbum['albums']['items'][0]['images'][0]['url']
 
-        await interaction.followup.send(embed=embed)
+            # album embed
+            embed = discord.Embed()
+            JST = timezone(timedelta(hours=+9), "JST")
+            embed.timestamp = datetime.now(JST)
+            embed.title = f"{album_title}"
+            embed.color = discord.Color.greyple()
+            embed.description = f"**Artist:** [{album_artist_name}]({album_artist_url})\n**Release Date:** {album_release_date}\n[Listen this album!]({album_url})"
+            embed.set_image(url=album_image)
+            await interaction.followup.send(embed=embed)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(
